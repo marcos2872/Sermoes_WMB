18f22cc1ad45cf6f35808321f4795ec4c08948ce
Marcos Brito <njsudev5@hotmail.com>
2023-12-09 14:54:31 -0300

    feat: axios

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {homeStyle} from './style.home';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import homeStyle from './style.home';
+import axios from 'axios';
+
+interface ISalmos {
+  text: string;
+  chapter: string;
+  number: string;
+}
 
 const Home: React.FC = () => {
+  const [salmos, setSalmos] = useState<ISalmos>();
+
+  useEffect(() => {
+    axios
+      .get('https://www.abibliadigital.com.br/api/verses/nvi/sl/random', {})
+      .then(({data}) => {
+        setSalmos({
+          chapter: data.chapter,
+          text: data.text,
+          number: data.number,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <SafeAreaView style={homeStyle.container}>
-      <Text>Home</Text>
+      <View style={homeStyle.salmoContainer}>
+        <Text style={homeStyle.title}>Salmo do Dia</Text>
+        <Text
+          style={
+            homeStyle.text
+          }>{`${salmos?.text} Salmos ${salmos?.chapter}:${salmos?.number}`}</Text>
+      </View>
     </SafeAreaView>
   );
 };
--- src/Screens/Home/style.home.ts
+++ src/Screens/Home/style.home.ts
@@ -1,9 +1,27 @@
 import {StyleSheet} from 'react-native';
 import {theme} from '../../Theme';
 
-export const homeStyle = StyleSheet.create({
+export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.black,
+    paddingTop: 20,
+  },
+
+  salmoContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+  },
+
+  title: {
+    color: theme.colors.white,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+
+  text: {
+    color: theme.colors.white,
   },
 });
